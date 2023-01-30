13c30af715aa9159e6898589d1f94ef2637a03da
DORA\Dora <dora@3fresources.com>
2023-01-30 14:48:26 +0800

    commit authenticate

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,20 +3,22 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-use App\Models\MngService;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return string|null
      */
     protected function redirectTo($request)
     {
-        $list = MngService::where('status','=',1)->get();
-        return view('auth.login',compact('list'));
-        
+        if (!$request->expectsJson()) {
+            return route('auth::login.show');
+        }
+
+        return null;
     }
 }
