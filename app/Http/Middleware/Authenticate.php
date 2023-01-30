0dea68d072486a8030f91fd74d85baf1014ddd03
DORA\Dora <dora@3fresources.com>
2023-01-30 15:56:10 +0800

    commit test

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -11,7 +11,7 @@ class Authenticate extends Middleware
      *
      * @param \Illuminate\Http\Request $request
      *
-     * @return string|null//
+     * @return string|null////
      */
     protected function redirectTo($request)
     {
