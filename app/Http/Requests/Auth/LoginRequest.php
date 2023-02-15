3a8f27e1f1710a55888b1d7d13e3ad563a85b130
DORA\Dora <dora@3fresources.com>
2023-02-15 09:26:15 +0800

    disable captcha

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -24,7 +24,7 @@ class LoginRequest extends FormRequest
         return [
             'email' => 'required|string|email',
             'password' => 'required|string',
-            'g-recaptcha-response' => 'required|captcha',
+           // 'g-recaptcha-response' => 'required|captcha',
            
         ];
     }
