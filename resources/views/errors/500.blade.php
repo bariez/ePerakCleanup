8a05bab3eeb9c4b44fda3d0ae40e7f079c6f1f0f
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-05-14 05:54:22 +0800

    Remove mytax error pages

--- resources/views/errors/500.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('errors::rendertohome')
-
-@section('code', 'Mytax Sedang Mengalami Gangguan')
-@section('title', __('Error'))
-
-<!-- @section('message', __('Sila layari pautan <a href="https://ez.hasil.gov.my/CI"> disini</a>')) -->
